8447d16ecd4f928defc93ef312617a3b221c724b
kingsleyonyelo <sabistore12@gmail.com>
2023-10-09 13:16:32 +0100

    feat stepper ui completed

--- src/components/StepperTitle.tsx
+++ src/components/StepperTitle.tsx
@@ -4,42 +4,44 @@ const StepperTitle = () => {
       <div className="ml-6 -mt-2">
         <span className="flex">
           <p className="font-Asap font-[23px] text-[#EFF1F1] ml-2 -mt-1">01</p>
-          <p className="font-Saira font-[16px] text-[#496970] -ml-1">
-            STEP ONE
-          </p>
+          <div className="flex flex-col justify-start items-start">
+            <p className="font-Saira font-[16px] text-[#496970] -ml-1">
+              STEP ONE
+            </p>
+            <p className="font-Asap text-[#27474E] font-[23px]">Your basket</p>
+          </div>
         </span>
-
-        <p className="font-Asap text-[#27474E] font-[23px]">Your basket</p>
       </div>
       <div className="ml-6 mt-[20px]">
         <span className="flex">
           <p className="font-Asap font-[23px] text-[#EFF1F1] ml-2 -mt-1">02</p>
-          <p className="font-Saira font-[16px] text-[#496970] -ml-1">
-            STEP TWO
-          </p>
+          <div className="flex flex-col justify-start items-start">
+            <p className="font-Saira font-[16px] text-[#496970]">STEP TWO</p>
+            <p className="font-Asap text-[#27474E] font-[23px]">Your details</p>
+          </div>
         </span>
-
-        <p className="font-Asap text-[#27474E] font-[23px]">Your details</p>
       </div>
       <div className="ml-6 mt-[20px]">
         <span className="flex">
           <p className="font-Asap font-[23px] text-[#EFF1F1] ml-2 -mt-1">03</p>
-          <p className="font-Saira font-[16px] text-[#496970] -ml-1">
-            STEP THREE
-          </p>
+          <div className="flex flex-col justify-start items-start">
+            <p className="font-Saira font-[16px] text-[#496970]">STEP THREE</p>
+            <p className="font-Asap text-[#27474E] font-[23px]">Payment</p>
+          </div>
         </span>
-
-        <p className="font-Asap text-[#27474E] font-[23px]">Payment</p>
       </div>
       <div className="ml-6 mt-[20px]">
         <span className="flex">
           <p className="font-Asap font-[23px] text-[#EFF1F1] ml-2 -mt-1">04</p>
-          <p className="font-Saira font-[16px] text-[#496970] -ml-1">
-            STEP FOUR
-          </p>
+          <div className="flex flex-col justify-start items-start">
+            <p className="font-Saira font-[16px] text-[#496970] leading-3">
+              STEP FOUR
+            </p>
+            <p className="font-Asap text-[#27474E] font-[23px]">
+              Order complete
+            </p>
+          </div>
         </span>
-
-        <p className="font-Asap text-[#27474E] font-[23px]">Order complete</p>
       </div>
     </div>
   );
