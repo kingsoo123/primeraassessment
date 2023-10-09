df7ca57a31fb80d9461b972dc45ea763c8177d92
kingsleyonyelo <sabistore12@gmail.com>
2023-10-09 13:07:00 +0100

    feat stepper ui completed

--- src/components/StepperTitle.tsx
+++ src/components/StepperTitle.tsx
@@ -1,45 +1,45 @@
 const StepperTitle = () => {
   return (
     <div>
-      <div className="ml-6 -mt-4">
+      <div className="ml-6 -mt-2">
         <span className="flex">
-          <p className="font-serif font-[23px] text-[#EFF1F1]">01</p>
-          <p className="font-Saira+Condensed font-[16px] text-[#496970]">
-            Step one
+          <p className="font-Asap font-[23px] text-[#EFF1F1] ml-2 -mt-1">01</p>
+          <p className="font-Saira font-[16px] text-[#496970] -ml-1">
+            STEP ONE
           </p>
         </span>
 
-        <p className="font-serif text-[#27474E] font-[23px]">Your basket</p>
+        <p className="font-Asap text-[#27474E] font-[23px]">Your basket</p>
       </div>
       <div className="ml-6 mt-[20px]">
         <span className="flex">
-          <p className="font-serif font-[23px] text-[#EFF1F1]">01</p>
-          <p className="font-Saira+Condensed font-[16px] text-[#496970]">
-            Step one
+          <p className="font-Asap font-[23px] text-[#EFF1F1] ml-2 -mt-1">02</p>
+          <p className="font-Saira font-[16px] text-[#496970] -ml-1">
+            STEP TWO
           </p>
         </span>
 
-        <p className="font-serif text-[#27474E] font-[23px]">Your details</p>
+        <p className="font-Asap text-[#27474E] font-[23px]">Your details</p>
       </div>
       <div className="ml-6 mt-[20px]">
         <span className="flex">
-          <p className="font-serif font-[23px] text-[#EFF1F1]">01</p>
-          <p className="font-Saira+Condensed font-[16px] text-[#496970]">
-            Step one
+          <p className="font-Asap font-[23px] text-[#EFF1F1] ml-2 -mt-1">03</p>
+          <p className="font-Saira font-[16px] text-[#496970] -ml-1">
+            STEP THREE
           </p>
         </span>
 
-        <p className="font-serif text-[#27474E] font-[23px]">Payment</p>
+        <p className="font-Asap text-[#27474E] font-[23px]">Payment</p>
       </div>
       <div className="ml-6 mt-[20px]">
         <span className="flex">
-          <p className="font-serif font-[23px] text-[#EFF1F1]">01</p>
-          <p className="font-Saira+Condensed font-[16px] text-[#496970]">
-            Step one
+          <p className="font-Asap font-[23px] text-[#EFF1F1] ml-2 -mt-1">04</p>
+          <p className="font-Saira font-[16px] text-[#496970] -ml-1">
+            STEP FOUR
           </p>
         </span>
 
-        <p className="font-serif text-[#27474E] font-[23px]">Order complete</p>
+        <p className="font-Asap text-[#27474E] font-[23px]">Order complete</p>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Asap", "sans-serif"],
-        serif: ["Asap", "Saira Condensed", "serif"],
+        Asap: ["Asap", "sans-serif"],
+        Saira: ["Saira+Condensed", "serif"],
       },
     },
   },
